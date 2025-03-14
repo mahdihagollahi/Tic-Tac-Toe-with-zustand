18c75fad998b3a9007fcb0aa520db97fcdba5194
mahdihaghollahi <mahdiaghollahi@gmail.com>
2025-03-14 16:40:35 +0330

    create-board

--- src/componnet/Square.tsx
+++ src/componnet/Square.tsx
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
-
-const Square = ({value , onSqureClick}) => {
+interface SquareValue{
+    value: string
+    onSqureClick:any
+}
+const Square:React.FC<SquareValue> = ({value , onSqureClick}) => {
   return (
     <Fragment>
     <button onClick={onSqureClick} className='inline-flex items-center justify-center p-0 bg-white border border-[#999] text-base font-bold'>
